4e708e2fed162067cb3bdc8d32d6f73ac95ef23e
Andrew Burton <chefquix@gmail.com>
2022-10-03 23:02:13 -0500

    adds some logical whizbang to try to deal with user input involving suites and street types

--- src/components/AddressAutoComplete.vue
+++ src/components/AddressAutoComplete.vue
@@ -8,7 +8,7 @@
       :clear-on-select="false"
       placeholder="Enter your street number and name."
       :loading="isLoading"
-      label="display_address"
+      label="search_address"
       name="display_address"
       :preserveSearch="true"
       @select="selectAddress"
@@ -64,10 +64,77 @@ export default {
         this.options = return_obj.data.map(row => {
           row.display_address = `${row.street_number} ${row.street_name} ${row.street_type}`;
           row.display_address += (row.street_direction) ? ' ' + row.street_direction : '';
+          //search address is so the auto-complete finds the things they've typed
+          row.search_address = '';
+          if (this.suite_number) {
+            row.search_address += this.suite_number;
+          }
+          row.search_address += `${row.street_number} ${row.street_name}`;
+          if (this.street_type_converted) {
+            //converted caught that its a valid street type, but put in whta they've actually typed so it catches in the autocomplete filter
+            row.search_address += ` ${this.street_type}`
+          }
           return row;
         })
       }
       this.isLoading = false;
+    },
+
+    convertStreetType(type) {
+      type = type.toLowerCase().replace('.','');
+      let dict = {
+        "AVE":["ave","avenue"],
+        "BAY":["bay"],
+        "BLVD":["blvd","boulevard",],
+        "CIR":["cir","circle"],
+        "CLOSE":["close"],
+        "COMMON":["common"],
+        "COVE":["cove"],
+        "CRES":["cres","crescent"],
+        "CROSS":["cross"],
+        "CRT":["crt","court"],
+        "DR":["dr","drive"],
+        "FWY":["fwy","freeway"],
+        "GATE":["gate"],
+        "GDN":["gdn","garden"],
+        "GDNS":["gdns","gardens"],
+        "GROVE":["grove"],
+        "HWY":["hwy","highway"],
+        "KEY":["key"],
+        "LANE":["lane"],
+        "MEWS":["mews"],
+        "PATH":["path"],
+        "PK":["pk","park","parc"],
+        "PKY":["pky","parkway"],
+        "PL":["pl","place"],
+        "PROM":["prom","promenade"],
+        "PT":["pt","point","pointe"],
+        "RD":["rd","road"],
+        "RIDGE":["ridge"],
+        "ROW":["row"],
+        "RUN":["run"],
+        "SQ":["sq","square","squares"],
+        "ST":["st","street"],
+        "TERR":["terr","terrace","terrasse"],
+        "TRAIL":["trail"],
+        "WALK":["walk","walkway"],
+        "WAY":["way"],
+      }
+      let reverse_dict = {};
+      Object.keys(dict).forEach(key => {
+        dict[key].forEach(value => {
+          reverse_dict[value] = key;
+        })
+      })
+      if (type in reverse_dict) {
+        return reverse_dict[type];
+      }
+      let filtered = Object.keys(reverse_dict).filter(key => key.startsWith(type));
+      if (filtered.length === 1) {
+        return reverse_dict[filtered[0]];
+      }
+      return '';
+
     }
   },
 
@@ -83,28 +150,59 @@ export default {
 
   computed: {
     searchable_address() {
-      return this.street_number + ' ' + this.street_name;
+      return this.street_number + ' ' + this.street_name + ' ' + this.street_type;
     },
 
     street_number() {
       if (!this.address) return '';
 
-      const address_pattern = /(\d+)/;
+      //const address_pattern = /(\d+)/;
+      const address_pattern = /(.*-\s*)?(\d+)/;
       let address_matches = this.address.match(address_pattern);
       if (address_matches === null) return '';
 
+      return address_matches[2];
+    },
+
+    suite_number() {
+      if (!this.address) return '';
+
+      const address_pattern = /(.*-\s*)?(\d+)/;
+      let address_matches = this.address.match(address_pattern);
+      if (address_matches === null) return '';
       return address_matches[1];
     },
 
     street_name() {
       if (!this.address) return '';
 
-      const address_pattern = /(\d+)\s+(.+)/;
+      const address_pattern = /(.*-\s*)?(\d+)\s+(.+)/;
       let address_matches = this.address.match(address_pattern);
       if (address_matches == null) return '';
-
-      return address_matches[2].trim();
+      if (this.street_type_converted) {
+        let pieces = address_matches[3].trim().split(/\s+/);
+        pieces.length = pieces.length - 1;
+        return pieces.join(' ');
+      }
+      return address_matches[3].trim();
     },
+
+    street_type_converted() {
+      return this.convertStreetType(this.street_type);
+    },
+
+    street_type() {
+      if (!this.address) return '';
+      const address_pattern = /(.*-)?(\d+)\s+(.+)/;
+      let address_matches = this.address.match(address_pattern);
+      if (address_matches == null) return '';
+      let pieces = address_matches[3].trim().split(/\s+/);
+      if (pieces.length > 1) {
+        return pieces[pieces.length -1 ];
+      }
+      return '';
+
+    }
 
   }
 }
